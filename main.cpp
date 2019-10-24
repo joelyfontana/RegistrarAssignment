ec97a22baf54393ced1466ffe2b82cdc89c66eea
Joely Fontana <fontana@chapman.edu>
2019-10-24 13:42:45 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/*Joely Fontana
+Student ID : 2313535
+Data Structures
+Section 1
+Assignment 4 - Registrar's Office*/
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	
+}
---
+++ window.cpp
@@ -0,0 +1,7 @@
+/*Joely Fontana
+Student ID : 2313535
+Data Structures
+Section 1
+Assignment 4 - Registrar's Office*/
+
+//use an array to take in the students - cpp or header file?? not sure which
